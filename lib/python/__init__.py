b837bcba2be4610ff3990ca914fbbfbcf936146d
Mikołaj Palkiewicz <xnerhu@gmail.com>
2025-06-22 13:15:30 +0200

    xd

--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -1,9 +1,6 @@
 from qpace_core import *
 
-import lib.content.ta as ta
-
-from lib.content import ta
-from lib.client import *
+from lib.python.client import *
 
 __version__ = "__BAZED::QPACE_VERSION__"
 __core__version__: str = _get_core_version()
