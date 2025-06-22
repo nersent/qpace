d34a079d498d1c36f52817588ebc9ca2f60266ae
Mikołaj Palkiewicz <xnerhu@gmail.com>
2025-06-22 12:52:28 +0200

    Xd

--- lib/content/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from dataclasses import dataclass
-from typing import Optional, Union, Literal, TypedDict, Any, Tuple, List
-from qpace_core import Ctx, Backtest
-from qpace_content import qpace_content as __lib__
-
-from . import ta
-__all__ = ['ta']
--- lib/python/__init__.py
+++
@@ -1,9 +0,0 @@
-from qpace_core import *
-
-import lib.content.ta as ta
-
-from lib.content import ta
-from lib.client import *
-
-__version__ = "__BAZED::QPACE_VERSION__"
-__core__version__: str = _get_core_version()
