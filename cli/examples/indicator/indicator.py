15d94d3074fcf10c214be2f3dcf4e3897e79cdb2
Mikołaj Palkiewicz <xnerhu@gmail.com>
2025-07-08 13:42:23 +0200

    update examples

--- cli/examples/indicator/indicator.py
+++ cli/examples/indicator/indicator.py
@@ -1,11 +1,8 @@
 import qpace as qp
 import __QPC_PYTHON_PACKAGE__ as pine
 
-ohlcv_path = "btc.csv"
-ohlcv = qp.Ohlcv.read_csv(ohlcv_path)
-ohlcv.timeframe = qp.Timeframe.Days(1)
-sym = qp.Sym.BTC_USD()
-ctx = qp.Ctx(ohlcv, sym)
+ohlcv = qp.Ohlcv.read_csv("btc.csv", qp.Timeframe.Days(1))
+ctx = qp.Ctx(ohlcv, qp.Sym.BTC_USD())
 
 res = pine.indicator.main(ctx.copy(), src="close", slow_length=30, fast_length=14)
 print(res["locals"]["ma"][0:90])
--- cli/examples/library/library.py
+++ cli/examples/library/library.py
@@ -1,11 +1,8 @@
 import qpace as qp
 import __QPC_PYTHON_PACKAGE__ as pine
 
-ohlcv_path = "btc.csv"
-ohlcv = qp.Ohlcv.read_csv(ohlcv_path)
-ohlcv.timeframe = qp.Timeframe.Days(1)
-sym = qp.Sym.BTC_USD()
-ctx = qp.Ctx(ohlcv, sym)
+ohlcv = qp.Ohlcv.read_csv("btc.csv", qp.Timeframe.Days(1))
+ctx = qp.Ctx(ohlcv, qp.Sym.BTC_USD())
 
 ma = pine.library.ma(ctx.copy(), ohlcv.close, 30, 14)
 print(ma[0:90])
